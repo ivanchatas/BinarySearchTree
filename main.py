d40833cbbb9f4a949d510122d029e4dd05c74788
Ivan Camilo Suarez Castro <ivanca.suarez@gmail.com>
2024-04-09 15:21:44 -0700

    add new code and new menu

--- main.py
+++ main.py
@@ -1,33 +1,67 @@
 import node
 
-# Driver Code
-if __name__ == '__main__':
+def default_code():
 	root = None
 	root = node.insert(root, 50)
 	node.insert(root, 30)
 	node.insert(root, 20)
 	node.insert(root, 40)
 	node.insert(root, 70)
-	node.insert(root, 60)
 	node.insert(root, 80)
+	node.insert(root, 60)
+	return root
 
-	# Key to be found
-	key = 6
+def menu(root):
+	print('\n******************************')
+	print('***          MENU          ***')
+	print('******************************')
+	print('***   1. Add new value     ***')
+	print('***   2. Search a value    ***')
+	print('***   3. Delete a value    ***')
+	print('***   4. Print pre-order   ***')
+	print('***   5. Print in-order    ***')
+	print('***   6. Print post-order  ***')
+	print('***   7. Default example   ***')
+	print('***                        ***')
+	print('******************************')
+	opt = int(input('Choose an option: '))
+	match opt:
+		case 1:
+			val = int(input('Enter a value to add: '))
+			if root is None:
+				root = node.insert(root, val)
+			else:
+				node.insert(root, val)
+		case 2:
+			key = int(input('Enter a value to search: '))
+			if node.search(root, key) is None:
+				print(key, "not found")
+			else:
+				print(key, "found")
+		case 3:
+			val = int(input('Enter a value to delete: '))
+			node.delete_node(root, val)
+		case 4:
+			if root is None:
+				print("The structure is empty")
+			else:
+				node.printBST_preOrder(root)
+		case 5:
+			if root is None:
+				print("The structure is empty")
+			else:
+				node.printBST_inOrder(root)
+		case 6:
+			if root is None:
+				print("The structure is empty")
+			else:
+				node.printBST_postOrder(root)
+		case 7:
+			root = default_code()
+		case _:
+			print("Please enter an exist option")
+	
+	menu(root)
 
-	# Searching in a BST
-	if node.search(root, key) is None:
-		print(key, "not found")
-	else:
-		print(key, "found")
-
-	key = 60
-
-	# Searching in a BST
-	if node.search(root, key) is None:
-		print(key, "not found")
-	else:
-		print(key, "found")
-
-	node.printBST_preOrder(root)
-	node.printBST_postOrder(root)
-	node.printBST_inOrder(root)
+root = None
+menu(root)
--- node.py
+++ node.py
@@ -1,64 +1,104 @@
 # Python3 function to search a given key in a given BST
 
 class Node:
-	# Constructor to create a new node
-	def __init__(self, key):
-		self.key = key
-		self.left = None
-		self.right = None
+    # Constructor to create a new node
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
 
 # A utility function to print
 # the tree with the parent node from the BST preOrder
 def printBST_preOrder(node):
-	print(f'({node.key})',end='')
-	if node.left != None:
-		printBST_preOrder(node.left)
-	if node.right != None:
-		printBST_preOrder(node.right)
+    print(f'({node.key})', end='')
+    if node.left is not None:
+        printBST_preOrder(node.left)
+    if node.right is not None:
+        printBST_preOrder(node.right)
+
 
 # A utility function to print
 # the tree with the parent node from the BST postOrder
 def printBST_postOrder(node):
-	if node.left != None:
-		printBST_postOrder(node.left)
-	if node.right != None:
-		printBST_postOrder(node.right)
-	print(f'({node.key})',end='')
+    if node.left is not None:
+        printBST_postOrder(node.left)
+    if node.right is not None:
+        printBST_postOrder(node.right)
+    print(f'({node.key})', end='')
+
 
 # A utility function to print
 # the tree with the parent node from the BST inOrder
 def printBST_inOrder(node):
-	if node.left != None:
-		printBST_inOrder(node.left)
-	print(f'({node.key})',end='')
-	if node.right != None:
-		printBST_inOrder(node.right)
+    if node.left is not None:
+        printBST_inOrder(node.left)
+    print(f'({node.key})', end='')
+    if node.right is not None:
+        printBST_inOrder(node.right)
+
 
 # A utility function to insert
 # a new node with the given key in BST
 def insert(node, key):
-	# If the tree is empty, return a new node
-	if node is None:
-		return Node(key)
+    # If the tree is empty, return a new node
+    if node is None:
+        return Node(key)
 
-	# Otherwise, recur down the tree
-	if key < node.key:
-		node.left = insert(node.left, key)
-	elif key > node.key:
-		node.right = insert(node.right, key)
+    # Otherwise, recur down the tree
+    if key < node.key:
+        node.left = insert(node.left, key)
+    elif key > node.key:
+        node.right = insert(node.right, key)
 
-	# Return the (unchanged) node pointer
-	return node
+    # Return the (unchanged) node pointer
+    return node
+
 
 # Utility function to search a key in a BST
 def search(root, key):
-	# Base Cases: root is null or key is present at root
-	if root is None or root.key == key:
-		return root
+    # Base Cases: root is null or key is present at root
+    if root is None or root.key == key:
+        return root
 
-	# Key is greater than root's key
-	if root.key < key:
-		return search(root.right, key)
+    # Key is greater than root's key
+    if root.key < key:
+        return search(root.right, key)
 
-	# Key is smaller than root's key
-	return search(root.left, key)
+    # Key is smaller than root's key
+    return search(root.left, key)
+
+
+def delete_node(root, key):
+    # If root doesn't exist, just return it
+    if not root:
+        return root
+
+    # Find the node in the left subtree if key value is less than root value
+    if root.key > key:
+        root.left = delete_node(root.left, key)
+    # Find the node in the right subtree if key value is greater than root value
+    elif root.key < key:
+        root.right = delete_node(root.right, key)
+    # Delete the node if root.keyue == key
+    else:
+        # If there is no right children delete the node and new root would be root.left
+        if not root.right:
+            return root.left
+        # If there is no right children delete the node and new root would be root.left
+        if not root.left:
+            return root.right
+
+        # If both left and right children exist in the node replace its value with
+        # the minimum value in the right subtree. Now delete that minimum node in
+        # the right subtree
+        temp_val = root.right
+        mini_val = temp_val.key
+        while temp_val.left:
+            temp_val = temp_val.left
+            mini_val = temp_val.key
+
+        # Delete the minimum node in right subtree
+        root.right = delete_node(root.right, root.key)
+
+    return root
